a3a70bf40b3c75b14ed6f90fd9545a5e70d17de9
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-08-24 12:47:54 -0400

    Modifying storeFocus function to accomdate for displaying data

--- storeFocus.py
+++ storeFocus.py
@@ -1,21 +1,25 @@
-import pandas as pd # For writing to CSV
+import os
+import pandas as pd
 from retrieveFocus import getFocused
-from datetime import datetime # Provides Timestamp for what was focused at what time.
+from datetime import datetime
 
 path_to_folder = "results"
+csv_file = path_to_folder + "/focus.csv"
 
 def storeFocus():
     # Get the current time
     now = datetime.now()
     # Get the current focused window
     focused = getFocused()
-    # Create a dict to store the focused window and the time.
+    # Create a dict to store the focused window and the time
     data = {
-        "Time Monitored": now.strftime("%H:%M:%S"),
-        "Focused": focused
+        "Time": [now.strftime("%H:%M:%S")],
+        "Focused": [focused]
     }
     # Create a dataframe from the dict
-    df = pd.DataFrame(data, index=[0])
-    # Create a CSV file to store the dataframe. Appends to EOF if exists.
-    df.to_csv(path_to_folder + "/focus.csv", index=False, mode="a", header=False)
+    df = pd.DataFrame(data)
+    # Check if the file exists to determine if headers should be written
+    file_exists = os.path.isfile(csv_file)
+    # Create a CSV file to store the dataframe. Append to EOF if it exists, else write headers.
+    df.to_csv(csv_file, index=False, mode="a", header=not file_exists)
 
