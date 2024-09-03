969fc35f341f1e33dc595e1f11aa1d7f02d38fbf
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-09-03 14:19:10 -0400

    PID additions. Need to fix negative bug

--- storeFocus.py
+++ storeFocus.py
@@ -1,29 +1,21 @@
 import os
 import pandas as pd
 from retrieveFocus import getFocused
-from datetime import datetime
 
 path_to_folder = "results"
 csv_file = path_to_folder + "/focus.csv"
 def storeFocus():
-    # Get the current time
-    now = datetime.now()
     # Get the current focused window
     file_exists = os.path.isfile(csv_file)
-    focused = getFocused()
+    data = getFocused() #HWND object
     if file_exists:
         prev = pd.read_csv(csv_file)['Focused'].iloc[-1]
         print(prev)
-        if prev == focused:
+        if prev == data['Focused']:
             return
     # Create a dict to store the focused window and the time
-    data = {
-        "Time": [now.strftime("%H:%M:%S")],
-        "Focused": [focused]
-    }
     # Create a dataframe from the dict
-    df = pd.DataFrame(data)
-    # Check if the file exists to determine if headers should be written
+    df = pd.DataFrame([data])
     # Create a CSV file to store the dataframe. Append to EOF if it exists, else write headers.
     df.to_csv(csv_file, index=False, mode="a", header=not file_exists)
 
