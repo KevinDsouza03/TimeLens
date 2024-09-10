2b94de6684430e8395c5112ae0ac1344a5b7a381
Kevin <kevindsouza476@gmail.com>
2024-09-09 22:26:39 -0400

    sorting comments for todo in each function

--- focus.py
+++ focus.py
@@ -92,6 +92,12 @@ def createTable():
 
 # This function processes every stat thats in the program scope.
 def programStats():
+    """
+    Per Program
+        1. Total time on Program DONE
+        2. Most used program in general *Can be accomplished by sorting via Total Time DONE
+        3. So, sum up until we reach a different program, then at the end of df we average that. Average_time, more of a total one.  DONE
+    """
     connection = sqlite3.connect(db_file)   
     df = pd.read_sql("SELECT * FROM focus_logs",connection)
     #The first thing is to add a tracking ended entry. So...
@@ -176,14 +182,35 @@ def programStats():
 
 def dayStats():
     #Todo
+    """
+    Per Day
+        1. Day specific time calculating (Grouping via Date)
+    """
     return
 
 def monthStats():
     #Todo
+    """
+    Per Month
+        1. Monthly Insights
+    """
     return
 
 def overallStats():
     #Todo
+    """
+    Overarching insights
+        1. Time distribution by Category: Classify specific apps (Work, Personal, Entertainment) and display how its broken down
+            - Categorize common applications. Make the user do it currently? Selecting an "intention" per focus session, and it categorizes it then. 
+
+        2. Context Switching, How often are we task switching
+            - Track time when "program" changes, and we can mark that down in the db. Then afterwards, can group by program and that gives us a 
+                "how long we stay focused on program"
+                    - 
+                    - Also have a col w a counter for each time we switch.
+
+        3. What time of the day are we the most productive? Morning, Afternoon, e.t.c
+    """
     return
 
 #Main function to call all others
@@ -193,35 +220,9 @@ def processFocus():
 
     *Each Seperation here will be a different table in db, as constantly re-calculating this is too inefficient
     Goes from specific to broader outlooks
-
-    Per Program
-    1. Total time on Program DONE
-    2. Most used program in general *Can be accomplished by sorting via Total Time DONE
-    3. So, sum up until we reach a different program, then at the end of df we average that. Average_time, more of a total one. 
-    
     Per Session: Between Session End entries
     1. 
 
-    Per Day
-    1. Day specific time calculating (Grouping via Date)
-    
-    Per Month
-    1. Monthly Insights
-
-    Overarching insights
-    1. Time distribution by Category: Classify specific apps (Work, Personal, Entertainment) and display how its broken down
-        - Categorize common applications. Make the user do it currently? Selecting an "intention" per focus session, and it categorizes it then. 
-
-    2. Context Switching, How often are we task switching
-        - Track time when "program" changes, and we can mark that down in the db. Then afterwards, can group by program and that gives us a 
-            "how long we stay focused on program"
-                - 
-                - Also have a col w a counter for each time we switch.
-
-    3. What time of the day are we the most productive? Morning, Afternoon, e.t.c
-    
-
-    
     potential changes:
     - Making a 'datetime' column in each program might be repetitive. Could maybe just make that the entire column? Or is it not that much
         repeated work?
