49447d57a05b7c024319a872dd5131b8ecb727f7
kevindsouza476@gmail.com <kevindsouza476@gmail.com>
2024-11-13 11:47:16 -0500

    tialwind install

---
+++ TimeLens/src/DashboardPage.jsx
@@ -0,0 +1,11 @@
+export default function DashboardPage() {
+
+    return (
+        <div className="flex flex-col gap-5 w-full">
+            <h1 className="font-bold text-4xl mx-6 text-center">TimeLens</h1>
+
+
+        </div>
+    )
+
+}
